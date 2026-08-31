2ab12f29d09c3341bf0347316c94c10ce80de817
Arpit Singh <arpitharshitbhai@gmail.com>
2026-08-31 23:46:04 +0530

    Day 9: oop concept

---
+++ day9/p1.py
@@ -0,0 +1,10 @@
+class Student:
+    def study(student):
+        print("Student is studying")
+
+    def attend_class(student):
+        print("Student is attending class")
+student1 = Student()
+
+student1.study()
+student1.attend_class()
